7c953af5894532819ee74790b829b58f2634e384
James Coglan <jcoglan@googlemail.com>
2009-06-11 19:54:59 +0100

    Remove a little line noise from grammar tests.

--- test/test_grammar.rb
+++ test/test_grammar.rb
@@ -2,85 +2,85 @@ require "test/unit"
 require "faye"
 
 class TestGrammar < Test::Unit::TestCase
-  include Faye
+  include Faye::Grammar
   
   def test_single_chars
-    assert Grammar::LOWALPHA =~ 'a'
-    assert Grammar::LOWALPHA !~ 'A'
-    assert Grammar::LOWALPHA !~ 'aa'
+    assert LOWALPHA =~ 'a'
+    assert LOWALPHA !~ 'A'
+    assert LOWALPHA !~ 'aa'
     
-    assert Grammar::UPALPHA !~ 'a'
-    assert Grammar::UPALPHA =~ 'A'
-    assert Grammar::UPALPHA !~ 'AA'
+    assert UPALPHA !~ 'a'
+    assert UPALPHA =~ 'A'
+    assert UPALPHA !~ 'AA'
     
-    assert Grammar::ALPHA =~ 'a'
-    assert Grammar::ALPHA =~ 'A'
-    assert Grammar::ALPHA !~ '0'
-    assert Grammar::ALPHA !~ 'aA'
+    assert ALPHA =~ 'a'
+    assert ALPHA =~ 'A'
+    assert ALPHA !~ '0'
+    assert ALPHA !~ 'aA'
     
-    assert Grammar::DIGIT =~ '0'
-    assert Grammar::DIGIT !~ '90'
-    assert Grammar::DIGIT !~ 'z'
+    assert DIGIT =~ '0'
+    assert DIGIT !~ '90'
+    assert DIGIT !~ 'z'
     
-    assert Grammar::ALPHANUM =~ '6'
-    assert Grammar::ALPHANUM =~ 'b'
-    assert Grammar::ALPHANUM !~ '6b'
-    assert Grammar::ALPHANUM !~ '/'
+    assert ALPHANUM =~ '6'
+    assert ALPHANUM =~ 'b'
+    assert ALPHANUM !~ '6b'
+    assert ALPHANUM !~ '/'
     
-    assert Grammar::MARK =~ '-'
-    assert Grammar::MARK =~ '@'
-    assert Grammar::MARK !~ '!-'
+    assert MARK =~ '-'
+    assert MARK =~ '@'
+    assert MARK !~ '!-'
   end
   
   def test_strings
-    assert Grammar::STRING =~ ''
-    assert Grammar::STRING =~ ' 4tv4 (($ !/~ ..* /) ___'
-    assert Grammar::STRING !~ ' 4tv4 (($ !/~ ..* /) _}_'
+    assert STRING =~ ''
+    assert STRING =~ ' 4tv4 (($ !/~ ..* /) ___'
+    assert STRING !~ ' 4tv4 (($ !/~ ..* /) _}_'
     
-    assert Grammar::TOKEN =~ '$a9b'
-    assert Grammar::TOKEN !~ ''
+    assert TOKEN =~ '$a9b'
+    assert TOKEN !~ ''
     
-    assert Grammar::INTEGER =~ '9'
-    assert Grammar::INTEGER =~ '09'
-    assert Grammar::INTEGER !~ '9.0'
-    assert Grammar::INTEGER !~ '9a'
+    assert INTEGER =~ '9'
+    assert INTEGER =~ '09'
+    assert INTEGER !~ '9.0'
+    assert INTEGER !~ '9a'
   end
   
   def test_channels
-    assert Grammar::CHANNEL_NAME =~ '/fo_o/$@()bar'
-    assert Grammar::CHANNEL_NAME !~ '/foo/$@()bar/'
-    assert Grammar::CHANNEL_NAME !~ 'foo/$@()bar'
-    assert Grammar::CHANNEL_NAME !~ '/fo o/$@()bar'
+    assert CHANNEL_NAME =~ '/fo_o/$@()bar'
+    assert CHANNEL_NAME !~ '/foo/$@()bar/'
+    assert CHANNEL_NAME !~ 'foo/$@()bar'
+    assert CHANNEL_NAME !~ '/fo o/$@()bar'
     
-    assert Grammar::CHANNEL_PATTERN =~ '/!!/$/*'
-    assert Grammar::CHANNEL_PATTERN =~ '/!!/$/**'
-    assert Grammar::CHANNEL_PATTERN !~ '/!!/$/**/'
-    assert Grammar::CHANNEL_PATTERN !~ '!!/$/**'
-    assert Grammar::CHANNEL_PATTERN !~ '/!!/$/**/*'
+    assert CHANNEL_PATTERN =~ '/!!/$/*'
+    assert CHANNEL_PATTERN =~ '/!!/$/**'
+    assert CHANNEL_PATTERN !~ '/!!/$/**/'
+    assert CHANNEL_PATTERN !~ '!!/$/**'
+    assert CHANNEL_PATTERN !~ '/!!/$/**/*'
   end
   
   def test_version
-    assert Grammar::VERSION =~ '9'
-    assert Grammar::VERSION =~ '9.a-delta4'
-    assert Grammar::VERSION !~ '9.a-delta4.'
-    assert Grammar::VERSION !~ 'K.a-delta4'
+    assert VERSION =~ '9'
+    assert VERSION =~ '9.a-delta4'
+    assert VERSION !~ '9.a-delta4.'
+    assert VERSION !~ 'K.a-delta4'
   end
   
   def test_ids
-    assert Grammar::CLIENT_ID =~ '9'
-    assert Grammar::CLIENT_ID =~ 'j'
-    assert Grammar::CLIENT_ID !~ ''
-    assert Grammar::CLIENT_ID =~ 'dfghs5r'
-    assert Grammar::CLIENT_ID !~ 'dfg_hs5r'
+    assert CLIENT_ID =~ '9'
+    assert CLIENT_ID =~ 'j'
+    assert CLIENT_ID !~ ''
+    assert CLIENT_ID =~ 'dfghs5r'
+    assert CLIENT_ID !~ 'dfg_hs5r'
   end
   
   def test_errors
-    assert Grammar::ERROR =~ '401::No client ID'
-    assert Grammar::ERROR !~ '401:No client ID'
-    assert Grammar::ERROR !~ '40::No client ID'
-    assert Grammar::ERROR !~ '40k::No client ID'
-    assert Grammar::ERROR =~ '402:xj3sjdsjdsjad:Unknown Client ID'
-    assert Grammar::ERROR =~ '403:xj3sjdsjdsjad,/foo/bar:Subscription denied'
-    assert Grammar::ERROR =~ '404:/foo/bar:Unknown Channel'
+    assert ERROR =~ '401::No client ID'
+    assert ERROR !~ '401:No client ID'
+    assert ERROR !~ '40::No client ID'
+    assert ERROR !~ '40k::No client ID'
+    assert ERROR =~ '402:xj3sjdsjdsjad:Unknown Client ID'
+    assert ERROR =~ '403:xj3sjdsjdsjad,/foo/bar:Subscription denied'
+    assert ERROR =~ '404:/foo/bar:Unknown Channel'
   end
 end
