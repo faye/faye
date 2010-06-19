2ffcd33d1ff928d04a7e3469d2e6f24a90a0c99e
James Coglan <jcoglan@gmail.com>
2010-06-20 00:45:56 +0100

    Refactor server-side WebSocket, implement DOM EventTarget API.

--- javascript/util/node/web_socket.js
+++ javascript/util/node/web_socket.js
@@ -10,7 +10,10 @@
  * For implementation reference:
  * http://dev.w3.org/html5/websockets/
  * http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-75
+ * http://www.w3.org/TR/DOM-Level-2-Events/events.html
  **/
+
+var Buffer = require('buffer').Buffer;
 
 Faye.WebSocket = Faye.Class({
   initialize: function(request) {
@@ -24,39 +27,50 @@ Faye.WebSocket = Faye.Class({
     this._handler = Faye.WebSocket.Protocol75;
     this._handler.handshake(this.url, this._request, this._socket);
     this.readyState = Faye.WebSocket.OPEN;
-    this.onopen();
     
-    var Buffer    = require('buffer').Buffer,
-        buffer    = [],
-        buffering = false;
-        self      = this;
+    var event = new Faye.WebSocket.Event();
+    event.initEvent('open', false, false);
+    this.dispatchEvent(event);
     
-    var handleChar = function(data) {
-      switch (data) {
-        case 0x00:    buffering = true;
-                      break;
-        
-        case 0xFF:    buffer = new Buffer(buffer);
-                      self.onmessage({data: buffer.toString('utf8', 0, buffer.length)});
-                      buffer = [];
-                      buffering = false;
-                      break;
-        
-        default:      if (buffering)
-                        buffer.push(data);
-      }
-    };
+    this._buffer = [];
+    this._buffering = false;
+    
+    var self = this;
     
     this._socket.addListener('data', function(data) {
       for (var i = 0, n = data.length; i < n; i++)
-        handleChar(data[i]);
+        self._handleChar(data[i]);
     });
   },
   
-  onopen:     function() {},
-  onmessage:  function() {},
-  onerror:    function() {},
-  onclose:    function() {},
+  _handleChar: function(data) {
+    switch (data) {
+      case 0x00:
+        this._buffering = true;
+        break;
+      
+      case 0xFF:
+        this._buffer = new Buffer(this._buffer);
+        
+        var event = new Faye.WebSocket.Event();
+        event.initEvent('message', false, false);
+        event.data = this._buffer.toString('utf8', 0, this._buffer.length);
+        
+        this.dispatchEvent(event);
+        
+        this._buffer = [];
+        this._buffering = false;
+        break;
+      
+      default:
+        if (this._buffering) this._buffer.push(data);
+    }
+  },
+  
+  onopen:     null,
+  onmessage:  null,
+  onerror:    null,
+  onclose:    null,
   
   send: function(data) {
     this._handler.send(this._socket, data);
@@ -65,17 +79,47 @@ Faye.WebSocket = Faye.Class({
   
   close: function() {},
   
-  // DOM EventTarget API, not implemented
-  addEventListener:     function(type, listener, useCapture) {},
-  removeEventListener:  function(type, listener, useCapture) {},
-  dispatchEvent:        function(evt) { return true }
+  addEventListener: function(type, listener, useCapture) {
+    this.addSubscriber(type, listener);
+  },
+  
+  removeEventListener: function(type, listener, useCapture) {
+    this.removeSubscriber(type, listener);
+  },
+  
+  dispatchEvent: function(event) {
+    event.target = event.currentTarget = this;
+    event.eventPhase = Faye.WebSocket.Event.AT_TARGET;
+    
+    this.publishEvent(event.type, event);
+    if (this['on' + event.type])
+      this['on' + event.type](event);
+  }
 });
+
+Faye.extend(Faye.WebSocket.prototype, Faye.Publisher);
 
 Faye.extend(Faye.WebSocket, {
   CONNECTING:   0,
   OPEN:         1,
   CLOSING:      2,
   CLOSED:       3,
+  
+  Event: Faye.extend(Faye.Class({
+    initEvent: function(eventType, canBubble, cancelable) {
+      this.type       = eventType;
+      this.bubbles    = canBubble;
+      this.cancelable = cancelable;
+    },
+    
+    stopPropagation: function() {},
+    preventDefault: function() {}
+    
+  }), {
+    CAPTURING_PHASE:  1,
+    AT_TARGET:        2,
+    BUBBLING_PHASE:   3
+  })
 });
 
 (function() {
