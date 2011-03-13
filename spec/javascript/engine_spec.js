76f4115b7f48fd120df456bd56f474da1212d920
James Coglan <jcoglan@gmail.com>
2011-03-13 12:46:39 +0000

    Refactor EngineSpec into async style using AsyncSteps.

--- spec/javascript/engine_spec.js
+++ spec/javascript/engine_spec.js
@@ -1,50 +1,112 @@
+JS.ENV.EngineSteps = JS.Test.asyncSteps({
+  create_client: function(name, resume) {
+    var clients = this._clients = this._clients || {}
+    this.engine.createClient(function(clientId) {
+      clients[name] = clientId
+      resume()
+    })
+  },
+  
+  destroy_client: function(name, resume) {
+    this.engine.destroyClient(this._clients[name])
+    resume()
+  },
+  
+  check_client_id: function(name, pattern, resume) {
+    this.assertMatch(pattern, this._clients[name])
+    resume()
+  },
+  
+  check_num_clients: function(n, resume) {
+    var ids = new JS.Set()
+    Faye.each(this._clients, function(name, id) { ids.add(id) })
+    this.assertEqual(n, ids.count())
+    resume()
+  },
+  
+  check_client_exists: function(name, exists, resume) {
+    var tc = this
+    tc.engine.clientExists(tc._clients[name], function(actual) {
+      tc.assertEqual(exists, actual)
+      resume()
+    })
+  },
+  
+  subscribe: function(name, channel, resume) {
+    this.engine.subscribe(this._clients[name], channel)
+    resume()
+  },
+  
+  unsubscribe: function(name, channel, resume) {
+    this.engine.unsubscribe(this._clients[name], channel)
+    resume()
+  },
+  
+  distribute: function(message, resume) {
+    this.engine.distribute(message)
+    resume()
+  },
+  
+  ping: function(name, resume) {
+    this.engine.ping(this._clients[name])
+    resume()
+  },
+  
+  clock_tick: function(time, resume) {
+    this.clock.tick(time)
+    resume()
+  },
+  
+  expect_announce: function(name, message, resume) {
+    this.expect(this.engine, "announce").given(this._clients[name], message)
+    resume()
+  },
+  
+  expect_no_announce: function(name, message, resume) {
+    this.expect(this.engine, "announce").given(this._clients[name], message).exactly(0)
+    resume()
+  }
+})
+
 JS.ENV.EngineSpec = JS.Test.describe("Pub/sub engines", function() { with(this) {
   include(JS.Test.Helpers)
   
   sharedExamplesFor("faye engine", function() { with(this) {
     include(JS.Test.FakeClock)
-    
-    define("makeClientId", function() {
-      var clientId = null
-      this.engine.createClient(function(id) { clientId = id })
-      return clientId
-    })
+    include(EngineSteps)
     
     define("options", function() { return {} })
     
     before(function() { with(this) {
-      this.clock.stub()
-      this.alice = makeClientId()
-      this.bob   = makeClientId()
-      this.cecil = makeClientId()
+      clock.stub()
+      create_client("alice")
+      create_client("bob")
+      create_client("carol")
     }})
     
     after(function() { with(this) {
-      this.clock.reset()
+      sync(clock.method("reset"))
     }})
     
     describe("createClient", function() { with(this) {
       it("returns a client id", function() { with(this) {
-        engine.createClient(function(id) {
-          assert( id )
-          assertMatch( /^[a-z0-9]+$/, id )
-        })
+        create_client("dave")
+        check_client_id("dave", /^[a-z0-9]+$/)
       }})
       
       it("returns a different id every time", function() { with(this) {
-        var ids = new JS.Set()
-        $R(1,10).forEach(function(i) { engine.createClient(ids.method('add')) })
-        assertEqual( 10, ids.count() )
+        $R(1,7).forEach(function(i) { create_client("client" + i) })
+        check_num_clients(10)
       }})
     }})
     
     describe("clientExists", function() { with(this) {
       it("returns true if the client id exists", function() { with(this) {
-        engine.clientExists(alice, assertYield(true))
+        check_client_exists("alice", true)
       }})
       
       it("returns false if the client id does not exist", function() { with(this) {
-        engine.clientExists("anything", assertYield(false))
+        check_client_exists("anything", false)
       }})
     }})
     
@@ -52,47 +114,43 @@ JS.ENV.EngineSpec = JS.Test.describe("Pub/sub engines", function() { with(this) 
       define("options", function() { return {timeout: 1} })
       
       it("removes a client if it does not ping often enough", function() { with(this) {
-        clock.tick(2000)
-        engine.clientExists(alice, assertYield(false))
+        clock_tick(2000)
+        check_client_exists("alice", false)
       }})
       
       it("prolongs the life of a client", function() { with(this) {
-        clock.tick(1000)
-        engine.ping(alice)
-        clock.tick(1000)
-        engine.clientExists(alice, assertYield(true))
+        clock_tick(1000)
+        ping("alice")
+        clock_tick(1000)
+        check_client_exists("alice", true)
+        clock_tick(1000)
+        check_client_exists("alice", false)
       }})
     }})
     
     describe("destroyClient", function() { with(this) {
       it("removes the given client", function() { with(this) {
-        engine.destroyClient(alice)
-        engine.clientExists(alice, assertYield(false))
+        destroy_client("alice")
+        check_client_exists("alice", false)
       }})
       
       describe("when the client has subscriptions", function() { with(this) {
         before(function() { with(this) {
-          this.inbox = {}
-          this.message = {'channel': '/messages/foo', 'data': 'ok'}
-          
-          engine.addSubscriber('messsage', function(clientId, message) {
-            inbox[clientId] = inbox[clientId] || []
-            inbox[clientId].push(message)
-          })
-          engine.subscribe(alice, "/messages/foo")
+          this.message = {"channel": "/messages/foo", "data": "ok"}
+          subscribe("alice", "/messages/foo")
         }})
         
         it("stops the client receiving messages", function() { with(this) {
-          engine.destroyClient(alice)
-          engine.distribute(message)
-          assertEqual( {}, inbox )
+          expect(engine, "announce").exactly(0)
+          destroy_client("alice")
+          distribute(message)
         }})
       }})
     }})
     
     describe("distribute", function() { with(this) {
       before(function() { with(this) {
-        this.message = {'channel': '/messages/foo', 'data': 'ok'}
+        this.message = {"channel": "/messages/foo", "data": "ok"}
       }})
       
       describe("with no subscriptions", function() { with(this) {
@@ -104,19 +162,19 @@ JS.ENV.EngineSpec = JS.Test.describe("Pub/sub engines", function() { with(this) 
       
       describe("with a subscriber", function() { with(this) {
         before(function() { with(this) {
-          engine.subscribe(alice, "/messages/foo")
+          subscribe("alice", "/messages/foo")
         }})
         
         it("delivers messages to the subscribed client", function() { with(this) {
-          expect(engine, "announce").given(alice, message)
-          engine.distribute(message)
+          expect_announce("alice", message)
+          distribute(message)
         }})
       }})
       
       describe("with a subscriber that is removed", function() { with(this) {
         before(function() { with(this) {
-          engine.subscribe(alice, "/messages/foo")
-          engine.unsubscribe(alice, "/messages/foo")
+          subscribe("alice", "/messages/foo")
+          unsubscribe("alice", "/messages/foo")
         }})
         
         it("does not deliver messages to unsubscribed clients", function() { with(this) {
@@ -127,46 +185,46 @@ JS.ENV.EngineSpec = JS.Test.describe("Pub/sub engines", function() { with(this) 
       
       describe("with multiple subscribers", function() { with(this) {
         before(function() { with(this) {
-          engine.subscribe(alice, "/messages/foo")
-          engine.subscribe(bob,   "/messages/bar")
-          engine.subscribe(cecil, "/messages/foo")
+          subscribe("alice", "/messages/foo")
+          subscribe("bob",   "/messages/bar")
+          subscribe("carol", "/messages/foo")
         }})
         
         it("delivers messages to the subscribed clients", function() { with(this) {
-          expect(engine, "announce").given(alice, message)
-          expect(engine, "announce").given(bob, message).exactly(0)
-          expect(engine, "announce").given(cecil, message)
-          engine.distribute(message)
+          expect_announce("alice", message)
+          expect_no_announce("bob", message)
+          expect_announce("carol", message)
+          distribute(message)
         }})
       }})
       
       describe("with a single wildcard", function() { with(this) {
         before(function() { with(this) {
-          engine.subscribe(alice, "/messages/*")
-          engine.subscribe(bob,   "/messages/bar")
-          engine.subscribe(cecil, "/*")
+          subscribe("alice", "/messages/*")
+          subscribe("bob",   "/messages/bar")
+          subscribe("carol", "/*")
         }})
         
         it("delivers messages to matching subscriptions", function() { with(this) {
-          expect(engine, "announce").given(alice, message)
-          expect(engine, "announce").given(bob, message).exactly(0)
-          expect(engine, "announce").given(cecil, message).exactly(0)
-          engine.distribute(message)
+          expect_announce("alice", message)
+          expect_no_announce("bob", message)
+          expect_no_announce("carol", message)
+          distribute(message)
         }})
       }})
       
       describe("with a double wildcard", function() { with(this) {
         before(function() { with(this) {
-          engine.subscribe(alice, "/messages/**")
-          engine.subscribe(bob,   "/messages/bar")
-          engine.subscribe(cecil, "/**")
+          subscribe("alice", "/messages/**")
+          subscribe("bob",   "/messages/bar")
+          subscribe("carol", "/**")
         }})
         
         it("delivers messages to matching subscriptions", function() { with(this) {
-          expect(engine, "announce").given(alice, message)
-          expect(engine, "announce").given(bob, message).exactly(0)
-          expect(engine, "announce").given(cecil, message)
-          engine.distribute(message)
+          expect_announce("alice", message)
+          expect_no_announce("bob", message)
+          expect_announce("carol", message)
+          distribute(message)
         }})
       }})
     }})
