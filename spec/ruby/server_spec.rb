3a837200c0a0bfba0d435d89272a2828880e0f37
James Coglan <jcoglan@gmail.com>
2011-03-14 22:28:31 +0000

    Unit test Ruby's Server#disconnect.

--- spec/ruby/server_spec.rb
+++ spec/ruby/server_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Faye::Server do
-  let(:engine) { Object.new }
+  let(:engine) { mock "engine" }
   let(:server) { Faye::Server.new }
   
   before do
@@ -246,5 +246,93 @@ describe Faye::Server do
     end
     
     # TODO fail if connectionType is not recognized
+  end
+  
+  describe :disconnect do
+    let(:client_id) { "fakeclientid" }
+    let(:message) {{"channel" => "/meta/disconnect",
+                    "clientId" => "fakeclientid"
+                  }}
+    
+    describe "with valid parameters" do
+      before do
+        engine.should_receive(:client_exists).with(client_id).and_yield true
+      end
+      
+      it "destroys the client" do
+        engine.should_receive(:destroy_client).with(client_id)
+        server.disconnect(message) {}
+      end
+      
+      it "returns a successful response" do
+        engine.stub(:destroy_client)
+        server.disconnect(message) do |response|
+          response.should == {
+            "channel"    => "/meta/disconnect",
+            "successful" => true,
+            "clientId"   => client_id
+          }
+        end
+      end
+      
+      describe "with a message id" do
+        before { message["id"] = "foo" }
+        
+        it "returns the same id" do
+          engine.stub(:destroy_client)
+          server.disconnect(message) do |response|
+            response.should == {
+              "channel"    => "/meta/disconnect",
+              "successful" => true,
+              "clientId"   => client_id,
+              "id"         => "foo"
+            }
+          end
+        end
+      end
+    end
+    
+    describe "with an unknown client" do
+      before do
+        engine.should_receive(:client_exists).with(client_id).and_yield false
+      end
+      
+      it "does not destroy the client" do
+        engine.should_not_receive(:destroy_client)
+        server.disconnect(message) {}
+      end
+      
+      it "returns an unsuccessful response" do
+        server.disconnect(message) do |response|
+          response.should == {
+            "channel"    => "/meta/disconnect",
+            "successful" => false,
+            "error"      => "401:fakeclientid:Unknown client"
+          }
+        end
+      end
+    end
+    
+    describe "missing clientId" do
+      before do
+        message.delete("clientId")
+        engine.should_receive(:client_exists).with(nil).and_yield false
+      end
+      
+      it "does not destroy the client" do
+        engine.should_not_receive(:destroy_client)
+        server.disconnect(message) {}
+      end
+      
+      it "returns an unsuccessful response" do
+        server.disconnect(message) do |response|
+          response.should == {
+            "channel"    => "/meta/disconnect",
+            "successful" => false,
+            "error"      => "402:clientId:Missing required parameter"
+          }
+        end
+      end
+    end
   end
 end
