f79ed63640c1acf1c7e5308b51031d35653ad0b7
James Coglan <jcoglan@gmail.com>
2011-03-27 17:13:44 +0100

    Remove over-large XHR library, just do what we need in the Transport.XHR class.

--- javascript/transport/xhr.js
+++ javascript/transport/xhr.js
@@ -1,17 +1,37 @@
 Faye.Transport.XHR = Faye.extend(Faye.Class(Faye.Transport, {
   request: function(message, timeout) {
-    var retry = this.retry(message, timeout);
+    var retry = this.retry(message, timeout),
+        path  = Faye.URI.parse(this._endpoint).pathname,
+        self  = this,
+        xhr   = Faye.ENV.ActiveXObject
+              ? new ActiveXObject("Microsoft.XMLHTTP")
+              : new XMLHttpRequest();
     
-    Faye.XHR.request('post', this._endpoint, Faye.toJSON(message), {
-      success:function(response) {
-        try {
-          this.receive(JSON.parse(response.text()));
-        } catch (e) {
+    xhr.open('POST', path, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== 4) return;
+      var status = xhr.status;
+      try {
+        if ((status >= 200 && status < 300) || status === 304 || status === 1223)
+          self.receive(JSON.parse(xhr.responseText));
+        else
           retry();
-        }
-      },
-      failure: retry
-    }, this);
+      } catch (e) {
+        retry();
+      } finally {
+        Faye.Event.detach(Faye.ENV, 'beforeunload', abort);
+        xhr.onreadystatechange = function() {};
+        xhr = null;
+      }
+    };
+    
+    var abort = function() { xhr.abort() };
+    Faye.Event.on(Faye.ENV, 'beforeunload', abort);
+    
+    xhr.send(Faye.toJSON(message));
   }
 }), {
   isUsable: function(endpoint, callback, scope) {
