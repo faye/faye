9db6822719a1182a4b993032755eef883f055f17
James Coglan <jcoglan@gmail.com>
2011-04-09 15:33:40 +0100

    Add script to automatically run the example app on a bunch of browsers with Terminus.

---
+++ examples/automate.rb
@@ -0,0 +1,45 @@
+dir = ::File.dirname(__FILE__)
+$LOAD_PATH.unshift(dir + '/../lib')
+
+require File.expand_path(dir + '/../vendor/terminus/lib/terminus')
+
+require File.expand_path(dir + '/rack/app')
+application = Rack::Builder.new {
+  use Faye::RackAdapter, :mount => '/bayeux', :timeout => 20
+  run Sinatra::Application
+}
+
+require 'capybara/dsl'
+Capybara.current_driver = :terminus
+Capybara.app = application.to_app
+extend Capybara
+
+NAMES = %w[alice bob carol dan erica]
+BROWSERS = {}
+
+Terminus.ensure_browsers 2
+
+Terminus.browsers.each_with_index do |browser, i|
+  BROWSERS[NAMES[i]] = browser
+  Terminus.browser = browser
+  visit '/'
+  fill_in 'username', :with => NAMES[i]
+  click_button 'Go'
+end
+
+BROWSERS.each do |name, sender|
+  BROWSERS.each do |at, target|
+    next if at == name
+    
+    Terminus.browser = sender
+    fill_in 'message', :with => "@#{at} Hello, world!"
+    click_button 'Send'
+    
+    Terminus.browser = target
+    unless page.has_content?("#{name}: @#{at} Hello, world!")
+      raise "Message did not make it from #{sender} to #{target}"
+    end
+  end
+end
+
+Terminus.return_to_dock
