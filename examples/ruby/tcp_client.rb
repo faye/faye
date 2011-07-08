0361b38052bb0f0ff62a46d6aa81f94101d89f46
James Coglan <jcoglan@gmail.com>
2011-07-08 04:46:40 -0700

    Parenthesize arguments in TCP client example.

--- examples/ruby/tcp_client.rb
+++ examples/ruby/tcp_client.rb
@@ -4,7 +4,7 @@ dir = File.dirname(__FILE__)
 require File.expand_path(dir + '/../../lib/faye')
 
 EM.run do
-  client = Faye::Client.new :host => 'localhost', :port => 3000
+  client = Faye::Client.new(:host => 'localhost', :port => 3000)
   
   client.subscribe '/members/tcpbot' do |message|
     p message
