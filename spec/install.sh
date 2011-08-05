4b0a5c028a6e56f7bccb22c426c52d99e12d13ee
James Coglan <jcoglan@gmail.com>
2011-08-05 11:19:38 +0100

    Tweak software install script.

--- spec/install.sh
+++ spec/install.sh
@@ -5,9 +5,9 @@
 # http://uec-images.ubuntu.com/releases/10.04/release/
 
 FAYE_BRANCH=master
-NODE_VERSION=0.4.8
+NODE_VERSION=0.4.10
 PHANTOM_VERSION=1.2
-REDIS_VERSION=2.2.11
+REDIS_VERSION=2.2.12
 RUBY_VERSION=1.9.2
 TSUNG_VERSION=1.3.3
 
@@ -17,24 +17,25 @@ sudo apt-get install build-essential g++ git-core curl wget \
                      apache2-utils erlang gnuplot \
                      libqt4-dev qt4-qmake xvfb
 
+# Install RVM and Ruby
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 echo "source \"\$HOME/.rvm/scripts/rvm\"" | tee -a ~/.bashrc
 source ~/.rvm/scripts/rvm
-rvm install 1.9.2
-rvm --default use 1.9.2
-
+rvm install $RUBY_VERSION
+rvm --default use $RUBY_VERSION
 echo "install: --no-rdoc --no-ri
 update: --no-rdoc --no-ri" | tee ~/.gemrc
 gem install rake bundler
 
+# Install nvm and Node
 cd ~
 git clone git://github.com/creationix/nvm.git ~/.nvm
 . ~/.nvm/nvm.sh
 echo ". ~/.nvm/nvm.sh" | tee -a ~/.bashrc
 nvm install v$NODE_VERSION
 nvm use v$NODE_VERSION
-npm install redis
 
+# Install Redis from source
 cd /usr/src
 sudo wget http://redis.googlecode.com/files/redis-$REDIS_VERSION.tar.gz
 sudo tar zxvf redis-$REDIS_VERSION.tar.gz
@@ -43,6 +44,7 @@ sudo make
 sudo ln -s /usr/src/redis-$REDIS_VERSION/src/redis-server /usr/bin/redis-server
 sudo ln -s /usr/src/redis-$REDIS_VERSION/src/redis-cli    /usr/bin/redis-cli
 
+# Install PhantomJS
 cd /usr/src
 sudo git clone git://github.com/ariya/phantomjs.git
 cd phantomjs
@@ -52,6 +54,7 @@ sudo make
 sudo ln -s /usr/src/phantomjs/bin/phantomjs /usr/bin/phantomjs
 echo "To use phantomjs, run DISPLAY=:1 Xvfb :1 -screen 0 1024x768x16"
 
+# Install Tsung and required Perl modules
 cd /usr/src
 sudo wget http://tsung.erlang-projects.org/dist/tsung-$TSUNG_VERSION.tar.gz
 sudo tar zxvf tsung-$TSUNG_VERSION.tar.gz
@@ -63,11 +66,13 @@ sudo ln -s /usr/lib/tsung/bin/tsung_stats.pl /usr/bin/tsung-stats
 echo "To use tsung-stats you need to 'install Template' from CPAN"
 sudo perl -MCPAN -eshell
 
+# Check out and build Faye project
 cd ~
 git clone git://github.com/jcoglan/faye.git
 cd faye
 git checkout $FAYE_BRANCH
 git submodule update --init --recursive
 bundle install
+npm install redis
 cd vendor/js.class && jake
 cd ../.. && jake
