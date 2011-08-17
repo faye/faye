8ae33feb6d35086b72a315dcd286b1c4dfe2c7ab
James Coglan <jcoglan@gmail.com>
2011-08-17 22:13:10 +0100

    Thin extensions shouldn't complain if Connection header is not present.

--- lib/thin_extensions.rb
+++ lib/thin_extensions.rb
@@ -53,6 +53,7 @@ class Thin::Request
   WEBSOCKET_RECEIVE_CALLBACK = 'websocket.receive_callback'.freeze
 
   def websocket?
+    @env['HTTP_CONNECTION'] and
     @env['HTTP_CONNECTION'].split(/\s*,\s*/).include?('Upgrade') and
     ['WebSocket', 'websocket'].include?(@env['HTTP_UPGRADE'])
   end
