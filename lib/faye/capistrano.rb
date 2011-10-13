2a2bd7f04bc34d0645d3a84e07b04903fd3707dc
Kir <razor.psp@gmail.com>
2011-10-13 23:26:21 +0400

    Ensure that rack file exists

--- lib/faye/capistrano.rb
+++ lib/faye/capistrano.rb
@@ -1,16 +1,18 @@
 # Capistrano task for Faye.
 #
 # Just add "require 'faye/capistrano'" in your Capistrano deploy.rb, and
-# Faye will be started after each new deployment.
+# Faye will be started/stoped after each new deployment.
 
 Capistrano::Configuration.instance(:must_exist).load do
-  _cset(:faye_init) { "faye.ru" }
+  _cset(:faye_config) { "#{current_path}/faye.ru" }
   _cset(:faye_pid) { "#{current_path}/tmp/pids/faye.pid" }
   
   namespace :faye do
     desc "Start Faye daemon"
     task :start do
-      run "cd #{current_path} && bundle exec rackup #{current_path}/#{faye_init} -s thin -E production -D --pid #{faye_pid}"
+      raise PathError, "Could not find Faye in #{faye_config}. Please, ensure that it exists." unless File.exists?(faye_config) # don't execute command if there is no rack file
+      
+      run "cd #{current_path} && bundle exec rackup #{faye_config} -s thin -E production -D --pid #{faye_pid}"
     end
 
     desc "Stop Faye"
