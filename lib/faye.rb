7d3370da86064276879499545c553818825e33a2
James Coglan <jcoglan@gmail.com>
2011-11-08 23:18:52 +0000

    Reorder a few functions.

--- lib/faye.rb
+++ lib/faye.rb
@@ -51,6 +51,11 @@ module Faye
   # http://www.w3.org/International/questions/qa-forms-utf-8.en.php
   UTF8_MATCH = /^([\x00-\x7F]|[\xC2-\xDF][\x80-\xBF]|\xE0[\xA0-\xBF][\x80-\xBF]|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}|\xED[\x80-\x9F][\x80-\xBF]|\xF0[\x90-\xBF][\x80-\xBF]{2}|[\xF1-\xF3][\x80-\xBF]{3}|\xF4[\x80-\x8F][\x80-\xBF]{2})*$/
   
+  def self.ensure_reactor_running!
+    Thread.new { EM.run } unless EM.reactor_running?
+    while not EM.reactor_running?; end
+  end
+  
   def self.random(bitlength = ID_LENGTH)
     limit    = 2 ** bitlength - 1
     max_size = limit.to_s(36).size
@@ -59,34 +64,16 @@ module Faye
     string
   end
   
+  def self.copy_object(object)
+    Marshal.load(Marshal.dump(object))
+  end
+  
   def self.to_json(value)
     case value
       when Hash, Array then JSON.unparse(value)
       when String, NilClass then value.inspect
       else value.to_s
     end
-  end
-  
-  def self.copy_object(object)
-    Marshal.load(Marshal.dump(object))
-  end
-  
-  def self.encode(string, validate_encoding = false)
-    if Array === string
-      return nil if validate_encoding and !valid_utf8?(string)
-      string = string.pack('C*')
-    end
-    return string unless string.respond_to?(:force_encoding)
-    string.force_encoding('UTF-8')
-  end
-  
-  def self.valid_utf8?(byte_array)
-    UTF8_MATCH =~ byte_array.pack('C*') ? true : false
-  end
-  
-  def self.ensure_reactor_running!
-    Thread.new { EM.run } unless EM.reactor_running?
-    while not EM.reactor_running?; end
   end
   
   def self.async_each(list, iterator, callback)
@@ -120,6 +107,19 @@ module Faye
       loop.call
     end
     resume.call
+  end
+  
+  def self.encode(string, validate_encoding = false)
+    if Array === string
+      return nil if validate_encoding and !valid_utf8?(string)
+      string = string.pack('C*')
+    end
+    return string unless string.respond_to?(:force_encoding)
+    string.force_encoding('UTF-8')
+  end
+  
+  def self.valid_utf8?(byte_array)
+    UTF8_MATCH =~ byte_array.pack('C*') ? true : false
   end
 end
 
