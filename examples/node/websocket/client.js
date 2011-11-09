c5a828e4430c2afad30d5d0f72104f152a6cf94f
James Coglan <jcoglan@gmail.com>
2011-11-09 22:43:00 +0000

    Formatting changes to examples/client.js.

--- examples/node/websocket/client.js
+++ examples/node/websocket/client.js
@@ -1,17 +1,18 @@
 var faye   = require('../../../build/faye-node'),
     port   = process.argv[2] || 7000,
-    socket = new faye.WebSocket.Client('socket://localhost:' + port + '/')
+    socket = new faye.WebSocket.Client('socket://localhost:' + port + '/');
 
 socket.onopen = function(event) {
-  console.log('open')
-  socket.send(JSON.stringify({channel: '/meta/subscribe', subscription: '/foo'}))
-}
+  console.log('open');
+  socket.send(JSON.stringify({channel: '/meta/subscribe', subscription: '/foo'}));
+};
 
 socket.onmessage = function(event) {
-  console.log('message', event.data)
-  socket.close(1002, 'Going away')
-}
+  console.log('message', event.data);
+  // socket.close(1002, 'Going away');
+};
 
 socket.onclose = function(event) {
-  console.log('close', event.code, event.reason)
-}
+  console.log('close', event.code, event.reason);
+};
+
