b3ab64d7bc7280a86f44bcd9ba2dedcaf11640b3
James Coglan <jcoglan@gmail.com>
2011-11-20 23:55:32 +0000

    Move error-handling examples into the main client example scripts.

--- examples/node/client.js
+++ examples/node/client.js
@@ -1,11 +1,11 @@
-// This script demonstrates a logger for the chat app. First, start
-// the chat server in one terminal then run this in another:
+// This script demonstrates a logger for the chat app. First, start the chat
+// server in one terminal then run this in another:
 // 
 //   $ node examples/node/server.js
 //   $ node examples/node/client.js
 // 
-// The client connects to the chat server and logs all messages
-// sent by all connected users.
+// The client connects to the chat server and logs all messages sent by all
+// connected users.
 
 var faye = require('../../build/faye-node'),
     
@@ -17,19 +17,31 @@ var faye = require('../../build/faye-node'),
 console.log('Connecting to ' + endpoint);
 var client = new faye.Client(endpoint);
 
-client.subscribe('/chat/*', function(message) {
+var subscription = client.subscribe('/chat/*', function(message) {
   var user = message.user;
   
-  client.publish('/members/' + user, {
+  var publication = client.publish('/members/' + user, {
     user:     'node-logger',
     message:  ' Got your message, ' + user + '!'
   });
+  publication.callback(function() {
+    console.log('[PUBLISH SUCCEEDED]');
+  });
+  publication.errback(function(error) {
+    console.log('[PUBLISH FAILED]', error);
+  });
+});
+
+subscription.callback(function() {
+  console.log('[SUBSCRIBE SUCCEEDED]');
+});
+subscription.errback(function(error) {
+  console.log('[SUBSCRIBE FAILED]', error);
 });
 
 client.bind('transport:down', function() {
   console.log('[CONNECTION DOWN]');
 });
-
 client.bind('transport:up', function() {
   console.log('[CONNECTION UP]');
 });
--- examples/ruby/client.rb
+++ examples/ruby/client.rb
@@ -1,11 +1,11 @@
-# This script demonstrates a logger for the chat app. First, start
-# the chat server in one terminal then run this in another:
+# This script demonstrates a logger for the chat app. First, start the chat
+# server in one terminal then run this in another:
 # 
 #   $ ruby examples/ruby/server.rb
 #   $ ruby examples/ruby/client.rb
 # 
-# The client connects to the chat server and logs all messages
-# sent by all connected users.
+# The client connects to the chat server and logs all messages sent by all
+# connected users.
 
 require 'rubygems'
 require File.expand_path('../../../lib/faye', __FILE__)
@@ -19,21 +19,33 @@ EM.run {
   puts "Connecting to #{endpoint}"
   client = Faye::Client.new(endpoint)
   
-  client.subscribe '/chat/*' do |message|
+  subscription = client.subscribe '/chat/*' do |message|
     user = message['user']
     
-    client.publish("/members/#{ user }", {
+    publication = client.publish("/members/#{ user }", {
       "user"    => "ruby-logger",
       "message" => "Got your message, #{ user }!"
     })
+    publication.callback do
+      puts "[PUBLISH SUCCEEDED]"
+    end
+    publication.errback do |error|
+      puts "[PUBLISH FAILED] #{error.inspect}"
+    end
+  end
+  
+  subscription.callback do
+    puts "[SUBSCRIBE SUCCEEDED]"
+  end
+  subscription.errback do |error|
+    puts "[SUBSCRIBE FAILED] #{error.inspect}"
   end
   
   client.bind 'transport:down' do
-    puts '[CONNECTION DOWN]'
+    puts "[CONNECTION DOWN]"
   end
-  
   client.bind 'transport:up' do
-    puts '[CONNECTION UP]'
+    puts "[CONNECTION UP]"
   end
 }
 
