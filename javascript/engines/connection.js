180f596d21d389dd26a3ffeea368f7185c0ccd92
James Coglan <jcoglan@gmail.com>
2012-01-15 19:02:51 +0000

    cometD requires messages pushed over WebSockets to be wrapped in an Array.

--- javascript/engines/connection.js
+++ javascript/engines/connection.js
@@ -7,7 +7,7 @@ Faye.Engine.Connection = Faye.Class({
   },
   
   deliver: function(message) {
-    if (this.socket) return this.socket.send(JSON.stringify(message));
+    if (this.socket) return this.socket.send(JSON.stringify([message]));
     this._inbox.push(message);
     this._beginDeliveryTimeout();
   },
