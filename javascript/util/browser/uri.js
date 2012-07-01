489a33485f3463e3a59808616388df81961ddc17
James Coglan <jcoglan@gmail.com>
2012-07-01 18:45:34 +0100

    Support HTTPS URLs in URI parser.

--- javascript/util/browser/uri.js
+++ javascript/util/browser/uri.js
@@ -36,11 +36,10 @@ Faye.URI = Faye.extend(Faye.Class({
     uri.hostname = a.hostname;
     uri.pathname = a.pathname;
     
-    if ((a.port === "0") || (a.port === "")) {
-      uri.port = "80";
-    } else {
+    if (a.port === '0' || a.port === '')
+      uri.port = (a.protocol === 'https:') ? '443' : '80';
+    else
       uri.port = a.port;
-    }
     
     var query = a.search.replace(/^\?/, ''),
         pairs = query ? query.split('&') : [],
