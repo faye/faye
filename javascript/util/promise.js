cb0e446af452c9320e9b4466399f3b344194f202
James Coglan <jcoglan@gmail.com>
2013-09-27 01:16:46 +0100

    Fix problem with IE10 complaining, like MS scheduling functions usually do, about setImmediate() not being called on the expected receiver.

--- javascript/util/promise.js
+++ javascript/util/promise.js
@@ -5,9 +5,9 @@ var timeout = setTimeout;
 
 var defer;
 if (typeof setImmediate === 'function')
-  defer = setImmediate;
+  defer = function(fn) { setImmediate(fn) };
 else if (typeof process === 'object' && process.nextTick)
-  defer = process.nextTick;
+  defer = function(fn) { process.nextTick(fn) };
 else
   defer = function(fn) { timeout(fn, 0) };
 
