c647be92b3f370d71e4ce1257346c4c7fc95de7e
James Coglan <jcoglan@gmail.com>
2016-07-29 06:51:32 +0100

    Return null from ws.create() if the browser does not support WebSocket. This prevents crashes in IE <10.

--- src/util/websocket/browser_websocket.js
+++ src/util/websocket/browser_websocket.js
@@ -4,6 +4,7 @@ var WS = global.MozWebSocket || global.WebSocket;
 
 module.exports = {
   create: function(url, protocols, options) {
+    if (typeof WS !== 'function') return null;
     return new WS(url);
   }
 };
