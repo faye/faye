50985f897f0cab58823a1cbbf2eead5a219a6df5
ryuuseijin <zamasu@fastmail.com>
2017-02-15 12:07:18 +1100

    Fix websockets in Safari

--- src/util/websocket/browser_websocket.js
+++ src/util/websocket/browser_websocket.js
@@ -4,7 +4,11 @@ var WS = global.MozWebSocket || global.WebSocket;
 
 module.exports = {
   create: function(url, protocols, options) {
-    if (typeof WS !== 'function') return null;
+    if (typeof WS !== 'function'
+        // In Safari versions <= 9 it is an 'object'
+        && typeof WS !== 'object') {
+        return null;
+    }
     return new WS(url);
   }
 };
